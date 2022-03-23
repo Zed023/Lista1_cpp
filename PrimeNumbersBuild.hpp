9ccc2967439ab49cf1194310ff5f367947971f64
Zed023 <57113386+Zed023@users.noreply.github.com>
2022-03-23 10:38:02 +0100

    Add files via upload

---
+++ PrimeNumbersBuild.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+class PrimeNumbers
+{
+    
+    public:
+    vector<bool> primes;
+    int n;
+    int m;
+
+    PrimeNumbers(int n1);
+    unsigned int liczba(int m1);
+};
